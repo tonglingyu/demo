178e0b18b322c66819007d9d083a3f8042088412
zhong an <anzhong70@gmail.com>
2018-05-10 18:21:52 +0900

    fghhjj

---
+++ src/hello/imooc/Helloword.java
@@ -0,0 +1,11 @@
+package hello.imooc;
+
+public class Helloword {
+
+	public static void main(String[] args) {
+		int a=3;
+		int b=a+3;
+		System.out.println(b);
+	}
+
+}
---
+++ src/hello/imooc/Month.java
@@ -0,0 +1,30 @@
+package hello.imooc;
+
+public class Month {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	int twoD[] []=new int [4] [5];
+	int i,j,k=0;
+	for(i=0;i<4;i++)
+	for(j=0;j<5;j++) {
+		twoD[i][j]=k;
+		k++;
+		}
+			
+	for(i=0;i<4;i++) {
+		for(j=0;j<5;j++) 
+	
+	System.out.print(twoD[i][j]+" ");
+		
+		System.out.println();
+		
+	
+	
+	
+	
+	}
+
+}
+}
